fb4f897989d08d140f8ac97ba06b4d4aa78f5f5c
bkkyy018 <bikashghadai5856@outlook.com>
2024-11-08 23:07:18 +0530

    large file has cloned

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingPage from "./LandingPage/LandingPage"
+import Signup from "./Authentication/Signup"
+import "./style.css"
+import Login from "./Authentication/Login"
+import { DemoContext } from "./DemoContext"
+import { useState } from "react"
+import HomePage from "./HomePage/HomePage"
+function App() {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <>
+      <BrowserRouter>
+      <DemoContext.Provider value={{isLogin,setIsLogin}}>
+      <Routes>
+            <Route  path="/" element={<LandingPage />}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/home" element={<HomePage/>} />
+          </Routes>
+           
+      </DemoContext.Provider>
+      </BrowserRouter>
+    </>
+  )
+}
+export default App
